d4bc092d4e6312618436d38ae559a80cc7298380
orpitaPesto <orpitadutta91@gmail.com>
2020-05-25 00:54:20 +0530

    Solve flip-args

--- js-exercises/flip-args/flipArgs.test.js
+++ js-exercises/flip-args/flipArgs.test.js
@@ -4,8 +4,8 @@ import { flipArgs } from "./flipArgs";
 
 describe("flipArgs", () => {
   it("should call the function with reversed order", () => {
-    expect(flipArgs(1, 2, 3, 4, 5)).toStrictEqual([5, 4, 3, 2, 1]);
-    expect(flipArgs("a", "b", "c", "d")).toStrictEqual(["d", "c", "b", "a"]);
-    expect(flipArgs("test", 100)).toStrictEqual(["test", 100]);
+    expect(flipArgs(1, 2, 3, 4, 5)).toEqual([5, 4, 3, 2, 1]);
+    expect(flipArgs("a", "b", "c", "d")).toEqual(["d", "c", "b", "a"]);
+    expect(flipArgs("test", 100)).toEqual([100, "test"]);
   });
 });
