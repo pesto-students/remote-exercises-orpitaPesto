51df81ff46dd24dcb5b45bc8974c480a21f7780d
orpitaPesto <orpitadutta91@gmail.com>
2020-05-25 18:48:27 +0530

    Solve add-big-integers

--- js-exercises/add-big-integers/addBigIntegers.js
+++ js-exercises/add-big-integers/addBigIntegers.js
@@ -1,16 +1,10 @@
-function addBigIntegers(intString) {
-
-    
-}
-
-export { addBigIntegers };
 function isInt(n){
     return Number(n) === n && n % 1 === 0;
-}function sum(stringNum)
-{
-    let numbersInArray = stringNum.split("\n");
+}
+
+function addBigIntegers(intString) {
+    let numbersInArray = intString.split("\n");
     let maximumDigitsBe = 0; 
-    console.log('numbersInArray :', numbersInArray);
     let digitsArray = [];
     numbersInArray.forEach(key => {
         let splitKey =key.split('');
@@ -19,30 +13,28 @@ function isInt(n){
         }
         digitsArray.push(splitKey);
     });
-    console.log('maximumDigitsBe :', maximumDigitsBe, ' digitsArray :', digitsArray);
+
     let sum = '';
     let carryOver = 0;
     for(let i=0; i < maximumDigitsBe; i++)
     {
         let digitsum = carryOver;
         digitsArray.forEach(key => {
-            console.log('Keys', key[i] ,i ,key, 'calcilated',key[(key.length-(i+1))]);
             if(key[(key.length-(i+1))])
             {
                 digitsum = digitsum + (+key[(key.length-(i+1))]);
             }
         });
-        console.log('DigitSum :', digitsum, ' i :', i);
         if(digitsum >= 10 ){
-        carryOver = digitsum % 10;
-        console.log('YUUUMMM :', ~~(digitsum / 10));
-        if(isInt(digitsum / 10)){
-            carryOver = digitsum / 10;
-            sum = 0+'' + sum
-        }
-        else{
-        sum = ~~(digitsum / 10) +'' + sum;
-        }
+        carryOver = ~~(digitsum / 10);
+
+            if(isInt(digitsum / 10)){
+                carryOver = digitsum / 10;
+                sum = 0+'' + sum
+            }
+            else{
+            sum = ~~(digitsum % 10) +'' + sum;
+            }
         
         
         }
@@ -50,7 +42,9 @@ function isInt(n){
             carryOver=0;
             sum = digitsum +'' + sum;
         }
-        console.log('DI44444 :', sum, 'carryOver', carryOver);
     }
-    console.log('SUM :', sum);
+    if(carryOver != 0){sum = carryOver + '' +sum}
+    return sum;
 }
+
+export { addBigIntegers };
